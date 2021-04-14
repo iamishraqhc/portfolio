af0ebfc778a00c2233da60a0d221424d658a5d78
Ishraq Haider Chowdhury <ishraq.h.c@gmail.com>
2021-04-14 13:11:03 +0200

    video background added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
 import React from "react";
 import image from "../image/barbados.jpg";
+import video from "../video/BlueOcean3.mp4"
 
 export default function Home() {
   return (
     <main>
-      <img
+      {/* <img
         src={image}
         alt="Mauritius"
         className="absolute object-cover w-full h-full"
-      />
+      /> */}
+      {/* <video loop autoplay className="absolute object-cover w-full h-full VideoTag">
+        <source src={video} type="video/mp4" />
+      </video> */}
+      <video className="absolute object-cover w-full h-full VideoTag" src={video} autoPlay loop muted> </video>
       <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
         <h1 className="text-6xl text-blue-100 font-bold cursive leading-none lg:leading-snug home-name">
           Hello! I'm Ishraq.
